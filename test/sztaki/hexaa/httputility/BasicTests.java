3788626b4ec86ec444be436b7d6e7855f355f73b
Bana Tibor <ede91bt@gmail.com>
2014-07-15 13:40:25 +0200

    TestSuites updated

--- test/sztaki/hexaa/httputility/BasicTests.java
+++ test/sztaki/hexaa/httputility/BasicTests.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite;
     sztaki.hexaa.httputility.apicalls.MethodNotAllowedTest.class,
     sztaki.hexaa.httputility.apicalls.services.ServicesSuite.class,
     sztaki.hexaa.httputility.apicalls.attributespecs.AttributespecsSuite.class,
-    sztaki.hexaa.httputility.apicalls.entitlements.EntitlementsSuite.class,})
+    sztaki.hexaa.httputility.apicalls.entitlements.EntitlementsSuite.class,
+    sztaki.hexaa.httputility.apicalls.entitlementpacks.EntitlementpacksSuite.class,})
 /**
  * TestSuite that runs with all the test suites and the few classes outside of
  * test suites to make sure that all of them were executed.
--- test/sztaki/hexaa/httputility/apicalls/entitlementpacks/EntitlementpacksSuite.java
+++ test/sztaki/hexaa/httputility/apicalls/entitlementpacks/EntitlementpacksSuite.java
@@ -3,10 +3,16 @@ package sztaki.hexaa.httputility.apicalls.entitlementpacks;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import sztaki.hexaa.httputility.apicalls.BasicTestSuite;
+import sztaki.hexaa.httputility.apicalls.entitlementpacks.entitlements.*;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-// TODO: update needed
-})
+    EntitlementpacksIsEmptyTest.class,
+    EntitlementpacksGetTest.class,
+    EntitlementpacksPutTest.class,
+    EntitlementpacksDeleteTest.class,
+    EntitlementpacksAddEntitlementsTest.class,
+    EntitlementpacksGetEntitlementsTest.class,
+    EntitlementpacksRemoveEntitlementsTest.class,})
 public class EntitlementpacksSuite extends BasicTestSuite {
 }
