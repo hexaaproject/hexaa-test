25cfc325d48f5facdada97f657fd062ead44b288
Bana Tibor <ede91bt@gmail.com>
2014-11-18 15:09:49 +0100

    Invitation accept fixed. Currently only invitation to organization is tested.

--- test/sztaki/hexaa/httputility/apicalls/invitations/InvitationsAcceptTokenTest.java
+++ test/sztaki/hexaa/httputility/apicalls/invitations/InvitationsAcceptTokenTest.java
@@ -1,26 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sztaki.hexaa.httputility.apicalls.invitations;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
-import org.skyscreamer.jsonassert.JSONParser;
+import sztaki.hexaa.httputility.Authenticator;
 import sztaki.hexaa.httputility.BasicCall;
 import sztaki.hexaa.httputility.Const;
+import sztaki.hexaa.httputility.ResponseTypeMismatchException;
 import sztaki.hexaa.httputility.Utility;
 import sztaki.hexaa.httputility.apicalls.CleanTest;
 
 /**
  * Test the GET method on the /api/invitations/{token}/accept/token call.
  */
-public class InvitationsAcceptTokenTest extends CleanTest{
-    
+public class InvitationsAcceptTokenTest extends CleanTest {
+
     /**
      * Print the class name on the output.
      */
@@ -30,7 +26,8 @@ public class InvitationsAcceptTokenTest extends CleanTest{
     }
 
     /**
-     * Creates an invitation.
+     * Creates an invitation, gets the token and authenticate a new user to
+     * accept it.
      */
     @BeforeClass
     public static void setUpClass() {
@@ -41,26 +38,53 @@ public class InvitationsAcceptTokenTest extends CleanTest{
                 "This is a test invitation to organization.",
                 0,
                 1);
-    }
-    
-    @Test
-    public void testInvitationsAcceptToken() {
-        Object response = JSONParser.parseJSON(persistent.call(Const.Api.INVITATIONS_ID, BasicCall.REST.GET));
-        
-        if (response instanceof JSONArray) {
-            fail("Got JSONArray for unknown reason: " + response.toString());
+
+        JSONObject jsonResponse;
+        try {
+            jsonResponse = persistent.getResponseJSONObject(Const.Api.INVITATIONS_ID, BasicCall.REST.GET);
+        } catch (ResponseTypeMismatchException e) {
+            fail(e.getFullMessage());
+            return;
         }
-        
-        JSONObject jsonResponse = (JSONObject) response;
+
+        Utility.Create.principal("invitationAcceptTokenTestFedid");
+        new Authenticator().authenticate("invitationAcceptTokenTestFedid");
         
         persistent.setToken(jsonResponse.getString("token"));
-        
-        System.out.println(persistent.call(Const.Api.INVITATIONS_TOKEN_ACCEPT_TOKEN, BasicCall.REST.GET));
-        
+    }
+    
+    /**
+     * Resets the fedid to the default.
+     */
+    @AfterClass
+    public static void tearDownClass() {
+        new Authenticator().authenticate(Const.HEXAA_FEDID);
+    }
+
+    @Test
+    public void testInvitationsAcceptToken() {
+//        Object response = JSONParser.parseJSON(persistent.call(Const.Api.INVITATIONS_ID, BasicCall.REST.GET));
+//        
+//        if (response instanceof JSONArray) {
+//            fail("Got JSONArray for unknown reason: " + response.toString());
+//        }
+
+        persistent.call(Const.Api.INVITATIONS_TOKEN_ACCEPT_TOKEN, BasicCall.REST.GET);
+
         try {
             assertEquals(Const.StatusLine.RedirectFound, persistent.getStatusLine());
+        } catch (AssertionError e) {
+            AssertErrorHandler(e);
+        }
+        
+        if (persistent.getHeader("Location") == null) {
+            fail("no location header; ");
+            return;
+        }
+        
+        try {
             assertEquals("http://hexaa.eduid.hu/hexaaui", persistent.getHeader("Location").getValue());
-        } catch(AssertionError e) {
+        } catch (AssertionError e) {
             AssertErrorHandler(e);
         }
     }
