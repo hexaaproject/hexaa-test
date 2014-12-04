104032cc73e7d4ae955dd5a9f5cfeb2553237132
Bana Tibor <ede91bt@gmail.com>
2014-12-04 17:34:29 +0100

    Trying to get the post attributes to life, some problems within the BasicCall

---
+++ test/AttributesPostTest.java
@@ -0,0 +1,41 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import sztaki.hexaa.httputility.Authenticator;
+import sztaki.hexaa.httputility.BasicCall;
+import sztaki.hexaa.httputility.Const;
+import sztaki.hexaa.httputility.ResponseTypeMismatchException;
+
+/**
+ * Tests the POST method on the /api/attributes call, that returns all the
+ * attributes without the normal authentication process for SSP.
+ */
+public class AttributesPostTest {
+
+    
+    
+    @Test
+    public void testAttributesPost() {
+        JSONObject json = new JSONObject();
+        
+        json.put("fedid", Const.HEXAA_FEDID);
+        json.put("entityid", "https://example.com/ssp");
+        json.put("apikey", new Authenticator().getAPIKey());
+        
+        BasicCall call = new BasicCall();
+        String response;
+        
+        response = call.call(Const.Api.ATTRIBUTES, BasicCall.REST.POST, json.toString());
+        
+        System.out.println(response);
+        
+        try {
+            assertEquals(Const.StatusLine.OK, call.getStatusLine());
+        } catch (AssertionError e) {
+            fail(e.getLocalizedMessage());
+        }
+    }
+}
