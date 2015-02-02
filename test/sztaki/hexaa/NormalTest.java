82cba010fd62be9c3a2e3dd1b7fc19e3a80e3748
Bana Tibor <ede91bt@gmail.com>
2015-02-02 14:13:17 +0100

    Tests fixed so all assertion fails will be seen.

--- test/sztaki/hexaa/NormalTest.java
+++ test/sztaki/hexaa/NormalTest.java
@@ -1,9 +1,11 @@
 package sztaki.hexaa;
 
+import java.util.ArrayList;
+
 import org.junit.BeforeClass;
-import sztaki.hexaa.Authenticator;
-import sztaki.hexaa.BasicCall;
-import sztaki.hexaa.Const;
+import org.junit.Rule;
+import org.junit.rules.ErrorCollector;
+import org.junit.runners.model.MultipleFailureException;
 
 /**
  * Parent class for all TestClasses for unified use of @BeforeClass, BasicCall,
@@ -21,9 +23,9 @@ public class NormalTest {
 	/**
 	 * This Rule allows the TestErrorCollector to bypass the AssertationErrors
 	 * so none of those will stop the running of the tests
-//	 */
-//	@Rule
-//	public TestErrorCollector collector = new TestErrorCollector();
+	 */
+	@Rule
+	public TestErrorCollector collector = new TestErrorCollector();
 
 	/**
 	 * Connects to the server via Runtime.exec and executes a server side script
@@ -48,69 +50,70 @@ public class NormalTest {
 	public void AssertErrorHandler(Throwable e) {
 		System.out.println(persistent.getStatusLine());
 		System.out.println(e.getLocalizedMessage());
-//		collector.addError(e);
+		collector.addError(e);
 	}
 
 	/**
 	 * ErrorCollector for the AssertationErrors happening during the execution
 	 * of the test classes, this way the TestSuite won't stop running after
 	 * minor errors, but finish and the errors are displayed all together.
-	 * Stores the error and the testclasses name.
+	 * Stores the error and the testclasses name. This class is necessary for
+	 * non-jsonassert assertions to work properly.
 	 */
-//	public class TestErrorCollector extends ErrorCollector {
-//
-//		/**
-//		 * List to collect the thrown errors for future use.
-//		 */
-//		private ArrayList<Throwable> errors = new ArrayList<>();
-//		/**
-//		 * String representing the method where the error was thrown. Not yet
-//		 * implemented.
-//		 */
-//		private String testMethodName = "";
-//
-//		/**
-//		 * Overriden from ErrorCollector.
-//		 *
-//		 * @throws Throwable
-//		 */
-//		@Override
-//		public void verify() throws Throwable {
-//			MultipleFailureException.assertEmpty(this.errors);
-//		}
-//
-//		/**
-//		 * Overriden from ErrorCollector.
-//		 *
-//		 * @param e
-//		 */
-//		@Override
-//		public void addError(Throwable e) {
-//			if (!this.errors.contains(e)) {
-//				this.errors.add(e);
-//			}
-//		}
-//
-//		/**
-//		 * Sets the n as name.
-//		 *
-//		 * @param n
-//		 *            new name.
-//		 */
-//		public void setName(String n) {
-//			if (n != null) {
-//				this.testMethodName = n;
-//			}
-//		}
-//
-//		/**
-//		 * Prints the error stack and the class name to the standard error.
-//		 */
-//		public void printErrorStack() {
-//			System.err.println(this.testMethodName);
-//			this.errors.stream().forEach((e) -> {
-//				System.err.println(e.getLocalizedMessage());
-//			});
-//		}
-//	}
+	public class TestErrorCollector extends ErrorCollector {
+
+		/**
+		 * List to collect the thrown errors for future use.
+		 */
+		private ArrayList<Throwable> errors = new ArrayList<>();
+		/**
+		 * String representing the method where the error was thrown. Not yet
+		 * implemented.
+		 */
+		private String testMethodName = "";
+
+		/**
+		 * Overriden from ErrorCollector.
+		 *
+		 * @throws Throwable
+		 */
+		@Override
+		public void verify() throws Throwable {
+			MultipleFailureException.assertEmpty(this.errors);
+		}
+
+		/**
+		 * Overriden from ErrorCollector.
+		 *
+		 * @param e
+		 */
+		@Override
+		public void addError(Throwable e) {
+			if (!this.errors.contains(e)) {
+				this.errors.add(e);
+			}
+		}
+
+		/**
+		 * Sets the n as name.
+		 *
+		 * @param n
+		 *            new name.
+		 */
+		public void setName(String n) {
+			if (n != null) {
+				this.testMethodName = n;
+			}
+		}
+
+		/**
+		 * Prints the error stack and the class name to the standard error.
+		 */
+		public void printErrorStack() {
+			System.err.println(this.testMethodName);
+			this.errors.stream().forEach((e) -> {
+				System.err.println(e.getLocalizedMessage());
+			});
+		}
+	}
 }
