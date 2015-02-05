61db5d8367648b63e66bccd2de8feca1af6487f8
Bana Tibor <ede91bt@gmail.com>
2015-02-05 12:46:31 +0100

    ServicesAttributespecsSetTest fixed up to be a normaltest

--- test/sztaki/hexaa/apicalls/services/attributespecs/ServicesAttributespecsSetTest.java
+++ test/sztaki/hexaa/apicalls/services/attributespecs/ServicesAttributespecsSetTest.java
@@ -1,21 +1,24 @@
 package sztaki.hexaa.apicalls.services.attributespecs;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import org.json.JSONArray;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
 import sztaki.hexaa.BasicCall;
 import sztaki.hexaa.Const;
+import sztaki.hexaa.NormalTest;
 import sztaki.hexaa.ResponseTypeMismatchException;
 import sztaki.hexaa.Utility;
-import sztaki.hexaa.CleanTest;
+import sztaki.hexaa.apicalls.attributespecs.AttributespecsGetTest;
 
 /**
  *
  */
-public class ServicesAttributespecsSetTest extends CleanTest {
+public class ServicesAttributespecsSetTest extends NormalTest {
 
 	/**
 	 * Print the class name on the output.
@@ -42,9 +45,31 @@ public class ServicesAttributespecsSetTest extends CleanTest {
 	public static void setUpClass() {
 		services = Utility.Create
 				.service(new String[] { "ServicesAttributespecsSetTest_service1" });
+		if (services.length() < 1) {
+			fail("Utility.Create.service(new String[] {\"ServicesAttributespecsSetTest_service1\"); did not succeed");
+		}
 		attributespecs = Utility.Create.attributespec(new String[] {
 				"ServicesAttributespecsSetTest_as1",
 				"ServicesAttributespecsSetTest_as2" }, "manager");
+		if (attributespecs.length() < 2) {
+			fail("Utility.Create.attributespec(new String[] {\"ServicesAttributespecsSetTest_as1\", \"ServicesAttributespecsSetTest_as2\" }, \"manager\"); did not succeed");
+		}
+	}
+
+	/**
+	 * Reverses the setUpClass and the creations during the test.
+	 */
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println("TearDownClass: "
+				+ AttributespecsGetTest.class.getSimpleName());
+		for (int i = 0; i < attributespecs.length(); i++) {
+			Utility.Remove.attributespec(attributespecs.getJSONObject(i)
+					.getInt("id"));
+		}
+		for (int i = 0; i < services.length(); i++) {
+			Utility.Remove.service(services.getJSONObject(i).getInt("id"));
+		}
 	}
 
 	/**
@@ -52,8 +77,11 @@ public class ServicesAttributespecsSetTest extends CleanTest {
 	 */
 	@Test
 	public void testServicesAttributespecsSet() {
-		Utility.Link.attributespecsToServiceByArray(1, new int[] { 1, 2 },
-				new boolean[] { true, true });
+		Utility.Link.attributespecsToServiceByArray(services.getJSONObject(0)
+				.getInt("id"), new int[] {
+				attributespecs.getJSONObject(0).getInt("id"),
+				attributespecs.getJSONObject(1).getInt("id") }, new boolean[] {
+				true, true });
 
 		try {
 			assertEquals(Const.StatusLine.Created,
@@ -67,10 +95,8 @@ public class ServicesAttributespecsSetTest extends CleanTest {
 		try {
 			jsonResponse = persistent.getResponseJSONArray(
 					Const.Api.SERVICES_ID_ATTRIBUTESPECS, BasicCall.REST.GET,
-					null, 1, 1);
+					null, services.getJSONObject(0).getInt("id"), 0);
 		} catch (ResponseTypeMismatchException ex) {
-			Logger.getLogger(ServicesAttributespecsSetTest.class.getName())
-					.log(Level.SEVERE, null, ex);
 			fail(ex.getFullMessage());
 			return;
 		}
@@ -84,8 +110,6 @@ public class ServicesAttributespecsSetTest extends CleanTest {
 								"attribute_spec_id"));
 			}
 		} catch (AssertionError e) {
-			System.out.println("expected: " + attributespecs + "\n"
-					+ "actual:   " + jsonResponse);
 			AssertErrorHandler(e);
 		}
 	}
