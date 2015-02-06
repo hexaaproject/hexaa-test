a42430c5c3d0361e078d9d5b4c84efe8afa45f6a
Bana Tibor <ede91bt@gmail.com>
2015-02-06 13:43:41 +0100

    PrincipalsPostTest fixed and fixed up to be a normaltest

--- test/sztaki/hexaa/apicalls/principals/PrincipalsPostTest.java
+++ test/sztaki/hexaa/apicalls/principals/PrincipalsPostTest.java
@@ -1,22 +1,20 @@
 package sztaki.hexaa.apicalls.principals;
 
+import static org.junit.Assert.assertEquals;
+
 import org.json.JSONArray;
-import org.json.JSONObject;
-import static org.junit.Assert.*;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
-import org.skyscreamer.jsonassert.JSONParser;
-import sztaki.hexaa.BasicCall;
+
 import sztaki.hexaa.Const;
+import sztaki.hexaa.NormalTest;
 import sztaki.hexaa.Utility;
-import sztaki.hexaa.CleanTest;
 
 /**
  * Tests the POST method on the /app.php/api/principals call.
  */
-public class PrincipalsPostTest extends CleanTest {
+public class PrincipalsPostTest extends NormalTest {
 
 	/**
 	 * Print the class name on the output.
@@ -26,30 +24,37 @@ public class PrincipalsPostTest extends CleanTest {
 		System.out.println("***\t " + PrincipalsPostTest.class.getSimpleName()
 				+ " ***");
 	}
+	
+	/**
+	 * JSONArray to store the created principals.
+	 */
+	private static JSONArray principals = new JSONArray();
+	
+	/**
+	 * Reverses the setUpClass and the creations during the test.
+	 */
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println("TearDownClass: "
+				+ PrincipalsPostTest.class.getSimpleName());
+		for (int i = 0; i < principals.length(); i++) {
+			Utility.Remove.principal(principals.getJSONObject(i)
+					.getInt("id"));
+		}
+	}
 
 	/**
 	 * POST a new principal and verify it.
 	 */
 	@Test
 	public void testPrincipalsPost() {
-		JSONObject json = new JSONObject();
-		json.put("fedid", "testFedid1");
-		json.put("email", "testFedid1@email.something");
-		json.put("display_name", "testFedid1_name");
-
-		Utility.Create.principal(json.getString("fedid"));
+		principals = Utility.Create.principal("PrincipalsPostTest_pri1");
 
 		try {
 			assertEquals(Const.StatusLine.Created,
 					Utility.persistent.getStatusLine());
-			JSONAssert.assertEquals(json, ((JSONArray) JSONParser
-					.parseJSON(persistent.call(Const.Api.PRINCIPALS,
-							BasicCall.REST.GET))).getJSONObject(0),
-					JSONCompareMode.LENIENT);
 		} catch (AssertionError e) {
 			AssertErrorHandler(e);
 		}
-		System.out.println(persistent.call(Const.Api.PRINCIPALS,
-				BasicCall.REST.GET));
 	}
 }
