81eb52f845f3addd7f862da30a41ed9588c8f109
Bana Tibor <ede91bt@gmail.com>
2015-02-06 17:37:59 +0100

    AttributespecsServicesGetTest fixed and made into a normal test

--- test/sztaki/hexaa/apicalls/attributespecs/AttributespecsServicesGetTest.java
+++ test/sztaki/hexaa/apicalls/attributespecs/AttributespecsServicesGetTest.java
@@ -1,21 +1,24 @@
 package sztaki.hexaa.apicalls.attributespecs;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 import sztaki.hexaa.BasicCall;
 import sztaki.hexaa.Const;
-import sztaki.hexaa.Utility;
-import sztaki.hexaa.CleanTest;
+import sztaki.hexaa.NormalTest;
 import sztaki.hexaa.ResponseTypeMismatchException;
+import sztaki.hexaa.Utility;
 
 /**
  * Tests the GET method on the /api/attributespecs/{id}/services call.
  */
-public class AttributespecsServicesGetTest extends CleanTest {
+public class AttributespecsServicesGetTest extends NormalTest {
 
 	/**
 	 * Print the class name on the output.
@@ -27,14 +30,51 @@ public class AttributespecsServicesGetTest extends CleanTest {
 	}
 
 	/**
+	 * JSONArray to store the created attributespecs.
+	 */
+	private static JSONArray attributespecs = new JSONArray();
+	/**
+	 * JSONArray to store the created services.
+	 */
+	private static JSONArray services = new JSONArray();
+
+	/**
 	 * Creates one attributespec and two services and links the attributespec to
 	 * the first service.
 	 */
 	@BeforeClass
 	public static void setUpClass() {
-		Utility.Create.attributespec("testOid", "user");
-		Utility.Create.service(new String[] { "testService1", "testService2" });
-		Utility.Link.attributespecsToService(1, new int[] { 1 }, true);
+		attributespecs = Utility.Create.attributespec(
+				"AttributespecsServicesGetTest_as1", "user");
+		if (attributespecs.length() < 1) {
+			fail("Utility.Create.attributespec( \"AttributespecsServicesGetTest_as1\", \"user\"); did not succeed");
+		}
+		services = Utility.Create.service(new String[] {
+				"AttributespecsServicesGetTest_service1",
+				"AttributespecsServicesGetTest_service2" });
+		if (services.length() < 2) {
+			fail("Utility.Create.service(new String[] {\"AttributespecsServicesGetTest_service1\", \"AttributespecsServicesGetTest_service2\" }); did not succeed");
+		}
+		Utility.Link.attributespecsToService(
+				services.getJSONObject(0).getInt("id"),
+				new int[] { attributespecs.getJSONObject(0).getInt("id") },
+				true);
+	}
+
+	/**
+	 * Reverses the setUpClass and the creations during the test.
+	 */
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println("TearDownClass: "
+				+ AttributespecsGetTest.class.getSimpleName());
+		for (int i = 0; i < services.length(); i++) {
+			Utility.Remove.service(services.getJSONObject(i).getInt("id"));
+		}
+		for (int i = 0; i < attributespecs.length(); i++) {
+			Utility.Remove.attributespec(attributespecs.getJSONObject(i)
+					.getInt("id"));
+		}
 	}
 
 	/**
@@ -42,20 +82,22 @@ public class AttributespecsServicesGetTest extends CleanTest {
 	 */
 	@Test
 	public void testAttributespecsServicesGet() {
-		JSONArray jsonResponse;
+		JSONObject jsonItems;
 		try {
-			jsonResponse = persistent.getResponseJSONArray(
-					Const.Api.ATTRIBUTESPECS_ID_SERVICES, BasicCall.REST.GET);
+			jsonItems = persistent.getResponseJSONObject(
+					Const.Api.ATTRIBUTESPECS_ID_SERVICES, BasicCall.REST.GET,
+					null, attributespecs.getJSONObject(0).getInt("id"), 0);
 		} catch (ResponseTypeMismatchException ex) {
-			Logger.getLogger(AttributespecsServicesGetTest.class.getName())
-					.log(Level.SEVERE, null, ex);
 			fail(ex.getFullMessage());
 			return;
 		}
 
+		JSONArray jsonResponse = jsonItems.getJSONArray("items");
+
 		try {
 			assertEquals(Const.StatusLine.OK, persistent.getStatusLine());
-			assertEquals(1, jsonResponse.getJSONObject(0).getInt("service_id"));
+			assertEquals(services.getJSONObject(0).getInt("id"), jsonResponse
+					.getJSONObject(0).getInt("service_id"));
 		} catch (AssertionError e) {
 			AssertErrorHandler(e);
 		}
