a3188eabfeb3641e1ac2c6099171ce8d2fd29e02
Bana Tibor <ede91bt@gmail.com>
2015-02-24 17:05:46 +0100

    first security domain test implementation, currently 500 internal error

--- test/sztaki/hexaa/apicalls/securitydomain/SecuritydomainPostTest.java
+++ test/sztaki/hexaa/apicalls/securitydomain/SecuritydomainPostTest.java
@@ -1,7 +1,76 @@
 package sztaki.hexaa.apicalls.securitydomain;
 
+import static org.junit.Assert.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import sztaki.hexaa.BasicCall;
+import sztaki.hexaa.Const;
 import sztaki.hexaa.NormalTest;
 
 public class SecuritydomainPostTest extends NormalTest {
+	
+	/**
+	 * Print the class name on the output.
+	 */
+	@BeforeClass
+	public static void classInformation() {
+		System.out.println("***\t "
+				+ SecuritydomainPostTest.class.getSimpleName() + " ***");
+	}
 
+	/**
+	 * JSONArray to store the created securitydomains.
+	 */
+	private static JSONArray domains = new JSONArray();
+	
+
+	/**
+	 * Creates two securitydomains.
+	 */
+	@BeforeClass
+	public static void setUpClass() {
+//		attributespecs = Utility.Create.attributespec(new String[] {
+//				"AttributespecsGetTest_as1", "AttributespecsGetTest_as2" },
+//				"user");
+//		if (attributespecs.length() < 2) {
+//			fail("Utility.Create.attributespec(new String[] {\"AttributespecsGetTest_as1\", \"AttributespecsGetTest_as2\" }, \"user\"); did not succeed");
+//		}
+	}
+	
+	/**
+	 * Reverses the setUpClass and the creations during the test.
+	 */
+	@AfterClass
+	public static void tearDownClass() {
+//		System.out.println("TearDownClass: "
+//				+ SecuritydomainPostTest.class.getSimpleName());
+//		for (int i = 0; i < attributespecs.length(); i++) {
+//			Utility.Remove.attributespec(attributespecs.getJSONObject(i)
+//					.getInt("id"));
+//		}
+	}
+	
+	@Test
+	public void testSecuritydomainPost() {
+		JSONObject json = new JSONObject();
+		json.put("name", "SecuritydomainPostTest_sd1");
+		json.put("scoped_key", "alternativeTestMasterKey");
+		
+		persistent.setAdmin();
+		persistent.call(Const.Api.SECURITYDOMAINS,BasicCall.REST.POST, json.toString());
+		
+		System.out.println(persistent.call());
+		
+		try {
+			assertEquals(Const.StatusLine.Created,persistent.getStatusLine());
+		} catch (AssertionError e) {
+			AssertErrorHandler(e);
+		}
+		
+	}
 }
