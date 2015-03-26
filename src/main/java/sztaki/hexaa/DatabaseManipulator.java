a85049950c23c6c184482d5d9873675636a4eed5
Bana Tibor <ede91bt@gmail.com>
2015-03-26 15:07:57 +0100

    Minor changes to dbmanipulator for better exception handling.

--- src/main/java/sztaki/hexaa/DatabaseManipulator.java
+++ src/main/java/sztaki/hexaa/DatabaseManipulator.java
@@ -18,45 +18,57 @@ public final class DatabaseManipulator {
 	public void dropDatabase() {
 		DataLoader data = new DataProp();
 
-		try {
+		Runtime rt = Runtime.getRuntime();
 
-			Runtime rt = Runtime.getRuntime();
+		Process proc;
 
-			Process proc;
-
-			if (new DataProp().getString("ssh_host").equals("localhost")) {
+		if (new DataProp().getString("ssh_host").equals("localhost")) {
+			try {
 				// Call for server side script if it runs on the server
-				proc = rt
-						.exec(new String[] { data.getString("sh_dir") + "databasedrop.sh" });
-			} else {
+				proc = rt.exec(new String[] { data.getString("sh_dir")
+						+ "databasedrop.sh" });
+			} catch (IOException ex) {
+				System.err
+						.println("The local execution failed before or during removing the mysql database. The database integrity my be damaged.");
+				return;
+			}
+		} else {
+			try {
 				// Call for server side script remotely
 				proc = rt.exec(new String[] {
 						"ssh",
 						data.getString("ssh_user") + "@"
 								+ data.getString("ssh_host"), "-p",
 						data.getString("ssh_port"), "~/databasedrop.sh" });
+			} catch (IOException ex) {
+				System.err
+						.println("The ssh connection failed before or during removing the mysql database. The database integrity my be damaged.");
+				return;
 			}
-
-			// any error message?
-			StreamGobbler errorGobbler = new StreamGobbler(
-					proc.getErrorStream(), "ERROR");
-
-			// any output?
-			StreamGobbler outputGobbler = new StreamGobbler(
-					proc.getInputStream(), "OUTPUT");
-
-			// kick them off
-			errorGobbler.start();
-			outputGobbler.start();
-
-			// any error???
-			int exitVal = proc.waitFor();
-			System.out.println("ExitValue: " + exitVal);
-
-		} catch (IOException | InterruptedException ex) {
-			System.err
-					.println("The ssh connection failed before or during removing the mysql database. The database integrity my be damaged.");
 		}
+
+		// any error message?
+		StreamGobbler errorGobbler = new StreamGobbler(proc.getErrorStream(),
+				"ERROR");
+
+		// any output?
+		StreamGobbler outputGobbler = new StreamGobbler(proc.getInputStream(),
+				"OUTPUT");
+
+		// kick them off
+		errorGobbler.start();
+		outputGobbler.start();
+
+		// any error???
+		int exitVal = 0;
+		try {
+			exitVal = proc.waitFor();
+		} catch (InterruptedException e) {
+			System.err
+					.println("The process was interrupted before or during removing the mysql database. The database integrity my be damaged.");
+		}
+		System.out.println("ExitValue: " + exitVal);
+
 	}
 
 	/**
@@ -66,45 +78,57 @@ public final class DatabaseManipulator {
 	public void dropCache() {
 		DataLoader data = new DataProp();
 
-		try {
+		Runtime rt = Runtime.getRuntime();
 
-			Runtime rt = Runtime.getRuntime();
+		Process proc;
 
-			Process proc;
-
-			if (new DataProp().getString("ssh_host").equals("localhost")) {
+		if (new DataProp().getString("ssh_host").equals("localhost")) {
+			try {
 				// Call for server side script if it runs on the server
-				proc = rt
-						.exec(new String[] { data.getString("sh_dir") + "cachedrop.sh" });
-			} else {
+				proc = rt.exec(new String[] { data.getString("sh_dir")
+						+ "cachedrop.sh" });
+			} catch (IOException ex) {
+				System.err
+						.println("The ssh connection failed before or during removing the cache.");
+				return;
+			}
+		} else {
+			try {
 				// Call for server side script remotely
 				proc = rt.exec(new String[] {
 						"ssh",
 						data.getString("ssh_user") + "@"
 								+ data.getString("ssh_host"), "-p",
 						data.getString("ssh_port"), "~/cachedrop.sh" });
+			} catch (IOException ex) {
+				System.err
+						.println("The ssh connection failed before or during removing the cache.");
+				return;
 			}
-
-			// any error message?
-			StreamGobbler errorGobbler = new StreamGobbler(
-					proc.getErrorStream(), "ERROR");
-
-			// any output?
-			StreamGobbler outputGobbler = new StreamGobbler(
-					proc.getInputStream(), "OUTPUT");
-
-			// kick them off
-			errorGobbler.start();
-			outputGobbler.start();
-
-			// any error???
-			int exitVal = proc.waitFor();
-			System.out.println("ExitValue: " + exitVal);
-
-		} catch (IOException | InterruptedException ex) {
-			System.err
-					.println("The ssh connection failed before or during removing the cache.");
 		}
+
+		// any error message?
+		StreamGobbler errorGobbler = new StreamGobbler(proc.getErrorStream(),
+				"ERROR");
+
+		// any output?
+		StreamGobbler outputGobbler = new StreamGobbler(proc.getInputStream(),
+				"OUTPUT");
+
+		// kick them off
+		errorGobbler.start();
+		outputGobbler.start();
+
+		// any error???
+		int exitVal = 0;
+		try {
+			exitVal = proc.waitFor();
+		} catch (InterruptedException e) {
+			System.err
+					.println("The proccess was interrupted before or during removing the cache.");
+		}
+		System.out.println("ExitValue: " + exitVal);
+
 	}
 
 	// mysql --user=root --password=pass hexaa -e
@@ -118,18 +142,23 @@ public final class DatabaseManipulator {
 		DataLoader data = new DataProp();
 		String output = new String();
 
-		try {
+		Runtime rt = Runtime.getRuntime();
 
-			Runtime rt = Runtime.getRuntime();
+		Process proc;
 
-			Process proc;
-
-			if (new DataProp().getString("ssh_host").equals("localhost")) {
-				// Call for server side script if it runs on the server
+		if (new DataProp().getString("ssh_host").equals("localhost")) {
+			// Call for server side script if it runs on the server
+			try {
 				proc = rt
 						.exec(new String[] { "mysql --user=root --password=pass hexaa -e \"SELECT enable_token FROM service;\"" });
-			} else {
-				// Call for server side script remotely
+			} catch (IOException ex) {
+				System.err
+						.println("The ssh connection failed before or during getting the service enable token.");
+				return "";
+			}
+		} else {
+			// Call for server side script remotely
+			try {
 				proc = rt
 						.exec(new String[] {
 								"ssh",
@@ -138,38 +167,35 @@ public final class DatabaseManipulator {
 								"-p",
 								data.getString("ssh_port"),
 								"mysql --user=root --password=pass hexaa -e \"SELECT enable_token FROM service;\"" });
+			} catch (IOException ex) {
+				System.err
+						.println("The ssh connection failed before or during getting the service enable token.");
+				return "";
 			}
-
-			// any error message?
-			StreamGobbler errorGobbler = new StreamGobbler(
-					proc.getErrorStream(), "ERROR");
-
-			// kick them off
-			errorGobbler.start();
-
-			try {
-				InputStreamReader isr = new InputStreamReader(
-						proc.getInputStream());
-				BufferedReader br = new BufferedReader(isr);
-				String line;
-				while ((line = br.readLine()) != null) {
-					if (line.contains("-")) {
-						output = line;
-					}
-					// System.out.println("SQL " + ">" + line);
-				}
-			} catch (IOException ioe) {
-			}
-
-			// any error???
-			@SuppressWarnings("unused")
-			int exitVal = proc.waitFor();
-			// System.out.println("ExitValue: " + exitVal);
-
-		} catch (IOException | InterruptedException ex) {
-			System.err
-					.println("The ssh connection failed before or during getting the service enable token.");
 		}
+
+		// any error message?
+		StreamGobbler errorGobbler = new StreamGobbler(proc.getErrorStream(),
+				"ERROR");
+
+		// kick them off
+		errorGobbler.start();
+
+		try {
+			InputStreamReader isr = new InputStreamReader(proc.getInputStream());
+			BufferedReader br = new BufferedReader(isr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (line.contains("-")) {
+					output = line;
+				}
+				// System.out.println("SQL " + ">" + line);
+			}
+		} catch (IOException ioe) {
+			System.err
+					.println("The inputstream from the mysql database was unreachable.");
+		}
+
 		return output;
 	}
 
@@ -197,6 +223,8 @@ public final class DatabaseManipulator {
 					System.out.println(type + ">" + line);
 				}
 			} catch (IOException ioe) {
+				System.err.println("The inputstream from the " + this.type
+						+ " could not be reached.");
 			}
 		}
 	}
